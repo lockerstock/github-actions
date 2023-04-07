146ba0df8670a30f4afab0beee61d3a92231bc5b
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2023-04-06 20:56:35 -0500

    temp change debug log to info

--- package-has-version/src/main.ts
+++ package-has-version/src/main.ts
@@ -32,7 +32,7 @@ async function run(): Promise<void> {
   const packageOwner = core.getInput('package_owner', {required: true})
   const packageVersion = core.getInput('package_version', {required: true})
 
-  core.debug(
+  core.info(
     JSON.stringify({packageName, packageOwner, packageVersion, packageType})
   )
 
