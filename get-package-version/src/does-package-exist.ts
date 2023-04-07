93de156766f717b1e11e2942fbfb7250bf18b2c7
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2023-04-07 17:00:45 -0500

    functionality of get-package-version

---
+++ get-package-version/src/does-package-exist.ts
@@ -0,0 +1,32 @@
+import * as core from '@actions/core'
+import {Octokit} from '@octokit/core'
+import {components} from '@octokit/openapi-types'
+
+interface PackageExists {
+  owner: string
+  name: string
+  type: components['schemas']['package']['package_type']
+}
+
+export async function doesPackageExist(
+  client: Octokit,
+  config: PackageExists
+): Promise<boolean> {
+  try {
+    const pkg = await getPackage(client, config)
+    return pkg.data.name === config.name
+  } catch (error) {
+    return false
+  }
+}
+
+async function getPackage(client: Octokit, {owner, name, type}: PackageExists) {
+  return await client.request(
+    'GET /orgs/{org}/packages/{package_type}/{package_name}',
+    {
+      org: owner,
+      package_name: name,
+      package_type: type
+    }
+  )
+}
