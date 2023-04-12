8c0269df8b279f426db8ad651820f94a08f0290c
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2023-04-11 21:52:14 -0500

    remove getRepo function

--- add-release-notes/src/main.ts
+++ add-release-notes/src/main.ts
@@ -1,14 +1,13 @@
 import * as core from '@actions/core';
 import {Octokit} from 'octokit';
 
-import {getRepo} from './action';
 import {getReleaseNotes} from './get-release-notes';
 import {injectMarkdownText} from './inject-markdown-text';
 import {updateReleaseNotes} from './update-release-notes';
 
 async function run(): Promise<void> {
   const owner = core.getInput('owner', {required: true});
-  const repo = core.getInput('repository', {required: false}) || getRepo();
+  const repo = core.getInput('repository', {required: true});
   const tag = core.getInput('tag', {required: true});
   const title = core.getInput('title', {required: true});
   const body = core.getInput('body', {required: true});
